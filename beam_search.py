de60f5a1ce09acab7af2c72552833682981f8aab
Slinking196 <matias.bugueno.b@mail.pucv.cl>
2024-04-27 23:46:29 -0400

    add new functions

--- beam_search.py
+++ beam_search.py
@@ -1,7 +1,8 @@
 import numpy as np
-from CPMP.cpmp_ml import generate_random_layout, Layout
 from keras.models import Model
-from keras.layers import Input, Dense, Flatten
+from keras import backend as K
+from CPMP.cpmp_ml import generate_random_layout, Layout
+from CPMP_Model.Model_CPMP import load_model
 from copy import deepcopy
 
 def get_ann_state(layout: Layout) -> np.ndarray:
@@ -28,7 +29,7 @@ def init_session(lays: list[Layout]) -> list[tuple]:
 def add_solution(solutions: dict, lay_solution: list[tuple]) -> dict:
     solutions_size = len(solutions)
 
-    if solutions_size == 0 or str(lay_solution[1]) in solutions:
+    if solutions_size == 0 or not str(lay_solution[1]) in solutions:
         solutions.update({str(lay_solution[1]): [lay_solution[0]]})
     else:
         solutions[str(lay_solution[1])].append(lay_solution[0])
@@ -76,6 +77,8 @@ def expanded_lays(model: Model, lays: list[tuple]):
                 new_lay.move((i, j))
                 child_lays.append((new_lay, lays[k][1]))
     
+    K.clear_session()
+
     return child_lays, pred_lays
 
 def get_dimensions(lst: list) -> list:
@@ -94,25 +97,77 @@ def get_dimensions(lst: list) -> list:
     
     return dimensions 
 
-def best_moves(lays: list[tuple], pred_lays: list[np.ndarray], w: int, threshold: int = 0.01) -> tuple:
-    cases = len(pred_lays)
+def get_lays_and_preds(cases):
+    cases_size = len(cases)
+    new_lays = []
+    new_pred = []
+
+    for i in range(cases_size):
+        lay_case = cases[str(i)]
+
+        for lay in lay_case['new_lays']:
+            new_lays.append(lay)
+
+        new_pred.append(np.array(lay_case['new_pred']))
+
+    return new_lays, new_pred 
+
+def sort_indices(lst, n):
+    # Crea una lista de índices ordenados de forma descendente basándose en los valores de la lista
+    sorted_indices = sorted(range(len(lst)), key=lambda x: lst[x], reverse=True)
+    
+    # Devuelve los n primeros índices
+    return sorted_indices[:n]
+
+def best_moves_v2(lays: list[tuple], child_pred_lays: list[np.ndarray], multiply_pred_lays: list[np.ndarray], w: int, threshold: int = 0.01) -> tuple:
+    pred_size = len(multiply_pred_lays)
     new_lays = []
     new_pred = []
 
     cont = 0
-    for k in range(cases):
-        pred_size = pred_lays[k].shape[0]
+    for k in range(pred_size):
+        pred_size = multiply_pred_lays[k].shape[0]
         temp_w = 0
-
+        
+        flag = True
         for i in range(pred_size):
-            if pred_lays[k][i] >= threshold and temp_w < w:
+            if flag: 
+                new_pred.append([])
+                flag = False
+
+            if multiply_pred_lays[k][i] >= threshold and temp_w < w:
                 new_lays.append(lays[cont])
-                new_pred.append(pred_lays[k][i])
+                new_pred[k].append(child_pred_lays[k][i])
                 temp_w += 1
 
             cont += 1
 
+        new_pred[k] = np.array(new_pred[k])
+
     return new_lays, new_pred
+
+def best_moves_v1(lays: list[tuple], child_pred_lays: list[np.ndarray], multiply_pred_lays: list[np.ndarray], w: int, threshold: int = 0.01) -> tuple:
+    pred_size = len(multiply_pred_lays)
+    cases = dict()
+
+    k = 0
+    for i in range(pred_size):
+        if len(cases) == 0 or not lays[k][1] in cases:
+            cases.update({str(lays[k][1]): {'new_lays': [], 'new_pred': []}})
+        
+        stack_size = multiply_pred_lays[i].shape[0]
+        temp_pred = deepcopy(child_pred_lays[i])
+        temp_pred = sort_indices(temp_pred.tolist(), w)
+        z = 0
+        for j in range(stack_size):
+            if multiply_pred_lays[i][j] >= threshold and z in temp_pred:
+                cases[str(lays[k][1])]['new_lays'].append(lays[k])
+                cases[str(lays[k][1])]['new_pred'].append(child_pred_lays[i][j])
+
+            z += 1
+            k += 1
+    
+    return get_lays_and_preds(cases)
 
 def multiply_predictions(pred_lays: list[np.ndarray], pred_child_lays: list[np.ndarray]) -> list[np.ndarray]:
     pred_lays_size = len(pred_lays)
@@ -120,12 +175,54 @@ def multiply_predictions(pred_lays: list[np.ndarray], pred_child_lays: list[np.n
 
     j = 0
     for k in range(pred_lays_size):
-        stack_size = pred_lays[0].shape[0]
+        stack_size = pred_lays[k].shape[0]
         
         for i in range(stack_size):
             multiply_pred.append(pred_child_lays[j] * pred_lays[k][i])
+            j += 1
     
     return multiply_pred
+
+def count_unsorted_elements(matrix):
+    # Cuenta el número de elementos desordenados en cada fila de una matriz
+    return sum(any(row[i] < row[i+1] for i in range(len(row)-1)) for row in matrix)
+
+def find_lower_bound(matrices):
+    # Encuentra la matriz con el menor número de elementos desordenados por fila
+    unsorted_counts = [count_unsorted_elements(matrix[0].stacks) for matrix in matrices]
+    least_unsorted_count = min(unsorted_counts)
+    
+    return least_unsorted_count
+
+def filter_lower_bound(lays: list[tuple], pred_lays: list[np.ndarray], lb: int, H: int):
+    pred_size = len(pred_lays)
+    new_lays = []
+    new_pred = []
+
+    k = 0
+    for i in range(pred_size):
+        lay_lb = count_unsorted_elements(lays[k][0].stacks)
+        stack_size = pred_lays[i].shape[0]
+        flag = True
+
+        for j in range(stack_size):
+            if flag:
+                new_pred.append([])
+                flag = False
+
+            if lb + H > lay_lb:
+                new_lays.append(lays[k])
+                new_pred[i].append(pred_lays[i][j])
+        
+        new_pred[i] = np.array(new_pred[i])
+
+    return new_lays, new_pred
+
+def show_lays(lays):
+    lays_size = len(lays)
+
+    for i in range(lays_size):
+        print(f'case {lays[i][1]}: {lays[i][0].stacks}')
 
 def beam_search(model: Model, lays: list[Layout] = None, H: int = 5, 
                 w: int = 3 , threshold: float = 0.01) -> list:
@@ -138,24 +235,56 @@ def beam_search(model: Model, lays: list[Layout] = None, H: int = 5,
     
     session_lays, pred_lays = expanded_lays(model, session_lays)
 
+    show_lays(session_lays)
+
+    session_lays, pred_lays = best_moves_v1(session_lays, pred_lays, pred_lays, w, threshold)
+
+    print('xd')
+    show_lays(session_lays)
+
     while True:
         session_lays, solutions = verify_solution(session_lays, solutions)
-        if len(session_lays) == 0: return solutions
+        if len(session_lays) == 0: break
+
+        print(get_dimensions(session_lays), get_dimensions(pred_lays))
+        print(len(solutions))
 
         child_lays, pred_child_lays = expanded_lays(model, session_lays)
         multiply_pred = multiply_predictions(pred_lays, pred_child_lays)
-        
-        session_lays, pred_lays = best_moves(child_lays, multiply_pred, w, threshold)
+        session_lays, pred_lays = best_moves_v1(child_lays, pred_child_lays, multiply_pred, w, threshold)
+
+        print('jaju')
+        show_lays(session_lays)
+
+        #lb = find_lower_bound(session_lays)
+        #session_lays, pred_lays = filter_lower_bound(session_lays, pred_lays, lb, H)
+
+        show_lays(session_lays)
 
     return solutions
+
+def show_results(cases: list[Layout], solutions: dict):
+    cases_size = len(cases)
+
+    for i in range(cases_size):
+        print(f'Case {i + 1}:')
+        print(f'State: {cases[i].stacks}')
+
+        case_solution = solutions[str(i)]
+        case_solution_size = len(case_solution)
+
+        for j in range(case_solution_size):
+            print(f'    solution {j}:')
+            print(f'        {case_solution[j].stacks}')
+
+
+if __name__ == "__main__":
+    model = load_model('./Models/model_5x5.keras')
+
+    cases = [generate_random_layout(5, 5, 15), generate_random_layout(5, 5, 15)]
+
+    solutions = beam_search(model, cases, w= 10, threshold= 0.01)
+    print(solutions)
+    show_results(cases, solutions)
     
-input_model = Input(shape= (5, 6))
-dense_1 = Dense(6)(input_model)
-dense_2 = Dense(6)(dense_1)
-flatten = Flatten()(dense_2)
-dense_3 = Dense(20, activation= 'sigmoid')(flatten)
-
-model = Model(inputs= input_model, outputs= dense_3)
-
-beam_search(model, [generate_random_layout(5, 5, 15), generate_random_layout(5, 5, 15)])
         
